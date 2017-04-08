7d6cb595b54119a46a2dde04bc685ca9fd163cbf
ahmed57928642 <ahmabdallah5@gmail.com>
2017-04-08 19:06:21 +0200

    Push all files

---
+++ index.ios.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { AppRegistry, Image, StyleSheet,
+  Text,
+  View } from 'react-native';
+
+export default class MyFirstProject extends Component  {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      
+      <View style={styles.container}>
+      <Image source={pic} style={{width: 193, height: 110}}/>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'blue'}} />
+      </View>
+      </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'steelblue',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+
+AppRegistry.registerComponent('MyFirstProject', () => MyFirstProject);
